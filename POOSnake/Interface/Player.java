74033aa22cb4b60abe34d010a99d074e70c70d42
LuisR <luis.rosa17@hotmail.com>
2024-04-22 13:06:43 +0100

    kjji

---
+++ POOSnake/Interface/Player.java
@@ -0,0 +1,32 @@
+package Interface;
+
+
+public class Player {
+
+    private int score;
+    private String name;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getNome() {
+        return name;
+    }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+
+    public Player (String name, int score) {
+        this.name=name;
+        this.score=score;
+    }
+
+
+    
+}
--- POOSnake/Interface/Rank.java
+++ POOSnake/Interface/Rank.java
@@ -1,17 +1,28 @@
 package Interface;
 
-public class Rank {
-    private List <Player> Players;   // Tipo - Jogador (int rankAtual, String nome, rank)
-    private int Score;
+import java.util.List;
 
-    private void updateRank(/*  player ?  */)
-    {
-        // Player.updateRank();
+public class Rank {
+    private List<Player> Players; // Tipo - Jogador (int scoreAtual, String nome, rank)
+
+    public Rank(List<Player> p) {
+        this.Players = p;
     }
 
-    private void showRank()
-    {
-        // sout Player.showRank();
+    private void updateRank(Player p, int score) {
+
+        //escrever no documento de texto (ha de ser um foreach com os players e um imprimir rank a mandar para o ficheiro)
+        //atualizar no objeto jogador
+        p.updateScore(score);
+    }
+
+    private void showRank(Player p) {
+        // sout p.showRank();
+
+    }
+
+    private void imprimirRank() {
+
     }
 
 }
