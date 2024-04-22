56eca9e3ad8b4c870f6a03c7129c8f2a491ed98e
justmepedro11lol <justmepedro11lol@gmail.com>
2024-04-22 15:34:53 +0100

    Create Cliente.java

---
+++ POOSnake/Ui/Cliente.java
@@ -0,0 +1,11 @@
+import java.lang.foreign.Arena;
+
+public class Main {
+    public static void main(String[] args) {
+
+     
+        Arena arena = new Arena(); {
+            
+        }
+    }
+}
