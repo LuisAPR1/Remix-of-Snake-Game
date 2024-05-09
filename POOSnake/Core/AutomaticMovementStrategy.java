a8944142c136c84875f25142b4bef5952ce2003f
LuisR <luis.rosa17@hotmail.com>
2024-05-09 20:48:21 +0100

    a

--- POOSnake/Core/AutomaticMovementStrategy.java
+++ POOSnake/Core/AutomaticMovementStrategy.java
@@ -1,14 +1,34 @@
 package Core;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class AutomaticMovementStrategy implements MovementStrategy {
-    @SuppressWarnings("unused")
     private Arena arena;
+    private Timer timer;
+    int counter;
 
     public AutomaticMovementStrategy(Arena arena) {
         this.arena = arena;
+        this.timer = new Timer();
+        startTimer();
+    }
+
+    private void startTimer() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                arena.Frame();
+                arena.calculateBestDirection();
+                System.out.println("AAAAAAAA" + counter++);
+            }
+        }, 0, 1000); // Executa a cada 1 segundo (1000 milissegundos)
     }
 
     @Override
     public void input() {
+       
     }
 }
+                
+
